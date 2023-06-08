8d14a4bebc37135834450dc64ccd4bdf99a7c909
Shashank R Pamadi <115693682+ShashankRPamadi@users.noreply.github.com>
2023-06-08 20:24:27 +0530

    Update Program.py

--- Program.py
+++ Program.py
@@ -1,1 +1,8 @@
-print("Hello World")
+def addition_calculator():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    result = num1 + num2
+    print("The sum of", num1, "and", num2, "is:", result)
+
+addition_calculator()
+
